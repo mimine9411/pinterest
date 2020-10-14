afb08092992de68f049e48182f9f49fdcbf70750
Amine Semlali <mimine9411@gmail.com>
2020-10-14 21:32:40 +0200

    V0.14.10

--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use App\Repository\PinRepository;
 use Doctrine\ORM\Mapping as ORM;
+use MongoDB\BSON\TimestampInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 /**
  * @ORM\Entity(repositoryClass=PinRepository::class)
+ * @ORM\HasLifecycleCallbacks
  */
 class Pin
 {
@@ -26,6 +29,16 @@ class Pin
      * @ORM\Column(type="text")
      */
     private $description;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default" : "CURRENT_TIMESTAMP"})
+     */
+    private $createAt;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default" : "CURRENT_TIMESTAMP"})
+     */
+    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -54,5 +67,42 @@ class Pin
         $this->description = $description;
 
         return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeInterface
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeInterface $createAt): self
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateTimestamps()
+    {
+        $lastupdated = new \DateTimeImmutable;
+        if($this->getCreateAt() === null) {
+            $this->setCreateAt($lastupdated);
+        }
+        $this->setUpdatedAt($lastupdated);
     }
 }
